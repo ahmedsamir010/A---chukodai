ddbae83996cffa5146c903a36d1ae6ba13d58e77
ahmed samir sakr <89707420+ahmedsamir010@users.noreply.github.com>
2022-01-23 15:20:35 +0200

    Add files via upload

---
+++ A - chukodai.cpp
@@ -0,0 +1,23 @@
+﻿//Link Problem==>https://codeforces.com/problemset/problem/427/A
+#include <iostream>
+#include<iomanip>
+#include<cmath>
+#include<string>
+#include<algorithm>
+using namespace std;
+#define ll long long
+#define ld long double
+#define pi 3.141592653589793238
+#define ahmed  ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL);
+#define no cout<<"NO"<<endl;
+#define yes cout<<"YES"<<endl;
+int counter[1000001] = {};
+int main()
+{
+    ahmed
+        string s; ll a, b; cin >> s >> a >> b; /*a -=1,b-=1;*/
+    swap(s[a-1], s[b-1]); cout << s;
+}
+//مهما الدنيا إتنيلت ضلمت لازم يبقى عندك زفت أمل و تعرف إن في نور في أخر أم النفق//
+
+        //اهو بنتنيل نستمر ف السعي رغم فقدان الزفت الشغف
